da15b15502ca98074626aa3eb5eea2fdfa8cf019
[Kenny] <75478619+Tzigeroni@users.noreply.github.com>
2023-06-10 19:49:21 +0300

    Dolev's Card

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+class Car:
+    def __init__(self, WheelNumber, PSI, FuelType ):
+        self.WheelNumber = WheelNumber
+        self.PSI = PSI
+        self.FuelType = FuelType
 
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def get_name(self):
-        return self.name
-    def get_age(self):
-        return self.age
+    def show(self):
+        print(self.WheelNumber, self.PSI, self.FuelType)
 
-    def set_age(self, age):
-        self.age = age
+        def get_WheelNumber(self):
+            return self.WheelNumber
 
+        def get_PSI(self):
+            return self.PSI
 
-d = Dog("Tim", 10)
-d.set_age(23)
-print(d.get_name(), d.get_age())
+        def get_FuelType(self):
+            return self.FuelType
+
+    def honk(self):
+            print("Wallah im Honking")
+
+    def drive(self):
+            print("Wallah im driving")
+
+car = Car("4", "30", "Diesel")
+car.show()
+car.drive()
+
+car2 = Car("4", "20", "Benzin")
+car2.show()
+car2.honk()
+
+car3 = Car("2", "25", "Soler")
+car3.show()
+car.drive()
+car.honk()
