6b9e191e3de2fc7c70cea0f2689692063af98008
[Kenny] <75478619+Tzigeroni@users.noreply.github.com>
2023-06-10 18:35:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
 class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
 
-    def add_one(self, x):
-        return x + 1
-    def bark(self):
-        print("bark")
+    def set_age(self, age):
+        self.age = age
 
-d = Dog()
-d.bark()
-print(d.add_one(5))
+
+d = Dog("Tim", 10)
+d.set_age(23)
+print(d.get_name(), d.get_age())
